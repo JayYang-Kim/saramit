b8aa6b4e96e123a7e7c33e25feaaafb200e727d4
HONGJA94 <pauldavidssss@gmail.com>
2019-03-08 19:59:32 +0900

    쪼금 작성해봄.

--- src/com/resume/ListDAO.java
+++ src/com/resume/ListDAO.java
@@ -1,11 +1,18 @@
-package com.resume;
-
-import java.sql.Connection;
-
-import com.util.DBConn;
-
-public class ListDAO {
-	private Connection conn=DBConn.getConnection();
-	
-	
-}
+/*
+ * package com.resume;
+ * 
+ * import java.sql.Connection; import java.sql.PreparedStatement; import
+ * java.sql.ResultSet; import java.util.ArrayList; import java.util.List;
+ * 
+ * import com.util.DBConn;
+ * 
+ * public class ListDAO { private Connection conn=DBConn.getConnection();
+ * 
+ * private List<ResumeDTO> listResume(String resumeCode){ List<ResumeDTO>
+ * list=new ArrayList<ResumeDTO>(); PreparedStatement pstmt=null; ResultSet
+ * rs=null; StringBuffer sb=new StringBuffer();
+ * 
+ * try { sb.append("SELECT * FROM("); sb.append(" SELECT ROWNUM num, tb.* fro")
+ * 
+ * } catch (Exception e) { // TODO: handle exception } return list; } }
+ */
